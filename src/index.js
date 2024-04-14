cc77259416004514ce706887650d62b48080e820
Yonatan Dvir <64538919+yonatan-dvir@users.noreply.github.com>
2024-04-15 01:53:10 +0300

    Refactored all of the app endpoints to use Async/Await

--- src/index.js
+++ src/index.js
@@ -11,81 +11,77 @@ const port = 3000;
 app.use(express.json());
 
 // Setup creation endpoint - create user
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const user = new User(req.body);
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
 // Setup reading endpoint - read all users
-app.get("/users", (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.send(users);
-    })
-    .catch((e) => {
-      res.status(500).send();
-    });
+app.get("/users", async (req, res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }catch(e){
+        res.status(500).send();
+    }
 });
 
 // Setup reading endpoint - read user by id
-app.get("/users/:id", (req, res) => {
-  const _id = req.params.id;
-  User.findById(_id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send();
-      }
-      res.send(user);
-    })
-    .catch((e) => {
-      res.status(500).send();
-    });
+app.get("/users/:id", async (req, res) => {
+    const _id = req.params.id
+    try{
+        const user = await User.findById(_id);
+        if (!user) {
+            return res.status(404).send();
+          }
+          res.send(user);
+    }catch(e){
+        res.status(500).send();
+    }
 });
 
 // Setup creation endpoint - create task
-app.post("/tasks", (req, res) => {
-  const task = new Task(req.body);
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-    });
+app.post("/tasks", async (req, res) => {
+    const task = new Task(req.body);
+
+    try{
+        await task
+        .save();
+        res.status(201).send(task);
+    }catch(e){
+        res.status(400).send(e);
+    }
+
 });
 
 // Setup reading endpoint - read all tasks
-app.get("/tasks", (req, res) => {
-  Task.find({})
-    .then((tasks) => {
-      res.send(tasks);
-    })
-    .catch((e) => {
-      res.status(500).send();
-    });
+app.get("/tasks", async (req, res) => {
+    try{
+        const tasks = await Task.find({});
+        res.send(tasks);
+    }catch(e){
+        res.status(500).send();
+    }
 });
 
 // Setup reading endpoint - read task by id
-app.get("/tasks/:id", (req, res) => {
-  const _id = req.params.id;
-  Task.findById(_id)
-    .then((task) => {
-      if (!task) {
-        return res.status(404).send();
-      }
-      res.send(task);
-    })
-    .catch((e) => {
-      res.status(500).send();
-    });
+app.get("/tasks/:id", async (req, res) => {
+    const _id = req.params.id;
+    try{
+        const task = await Task.findById(_id);
+        if (!task) {
+            return res.status(404).send();
+          }
+          res.send(task);
+    }catch(e){
+        res.status(500).send();
+    }
+  
 });
 
 app.listen(port, () => {
