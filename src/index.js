bac363c75d5acd2f980537df2656cc1d28b07c68
Yonatan Dvir <64538919+yonatan-dvir@users.noreply.github.com>
2024-04-15 00:47:11 +0300

    Added setups for creating users and tasks, setup for reading all users and tasks, setup for reading users and tasks by id

--- src/index.js
+++ src/index.js
@@ -10,29 +10,81 @@ const port = 3000;
 // Use express automatically json convert
 app.use(express.json());
 
-// Setup the user creation endpoint
+// Setup creation endpoint - create user
 app.post("/users", (req, res) => {
   const user = new User(req.body);
   user
     .save()
     .then(() => {
-      res.send(user);
+      res.status(201).send(user);
     })
     .catch((e) => {
       res.status(400).send(e);
     });
 });
 
-// Setup the task creation endpoint
+// Setup reading endpoint - read all users
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
+// Setup reading endpoint - read user by id
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
+// Setup creation endpoint - create task
 app.post("/tasks", (req, res) => {
   const task = new Task(req.body);
   task
     .save()
     .then(() => {
-      res.send(task);
+      res.status(201).send(task);
     })
     .catch((e) => {
       res.status(400).send(e);
+    });
+});
+
+// Setup reading endpoint - read all tasks
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.send(tasks);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
+// Setup reading endpoint - read task by id
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send();
+      }
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(500).send();
     });
 });
 
