222405d31137ed7d8adfa7f08e3134afd698043c
Yonatan Dvir <64538919+yonatan-dvir@users.noreply.github.com>
2024-04-15 09:55:14 +0300

    Added setups for updating user and task by id

--- src/index.js
+++ src/index.js
@@ -23,65 +23,115 @@ app.post("/users", async (req, res) => {
 
 // Setup reading endpoint - read all users
 app.get("/users", async (req, res) => {
-    try{
-        const users = await User.find({});
-        res.send(users);
-    }catch(e){
-        res.status(500).send();
-    }
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
 // Setup reading endpoint - read user by id
 app.get("/users/:id", async (req, res) => {
-    const _id = req.params.id
-    try{
-        const user = await User.findById(_id);
-        if (!user) {
-            return res.status(404).send();
-          }
-          res.send(user);
-    }catch(e){
-        res.status(500).send();
+  const _id = req.params.id;
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
     }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+// Setup updating endpoint - update user by id
+app.patch("/users/:id", async (req, res) => {
+  // Send error if the update is unknown
+  const updates = Object.keys(req.body);
+  const allowedUpdated = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdated.includes(update)
+  );
+  if (!isValidOperation) {
+    res.status(400).send("Error - Invalid update!");
+  }
+  const _id = req.params.id;
+  try {
+    const user = await User.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
 // Setup creation endpoint - create task
 app.post("/tasks", async (req, res) => {
-    const task = new Task(req.body);
+  const task = new Task(req.body);
 
-    try{
-        await task
-        .save();
-        res.status(201).send(task);
-    }catch(e){
-        res.status(400).send(e);
-    }
-
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
 // Setup reading endpoint - read all tasks
 app.get("/tasks", async (req, res) => {
-    try{
-        const tasks = await Task.find({});
-        res.send(tasks);
-    }catch(e){
-        res.status(500).send();
-    }
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
 // Setup reading endpoint - read task by id
 app.get("/tasks/:id", async (req, res) => {
-    const _id = req.params.id;
-    try{
-        const task = await Task.findById(_id);
-        if (!task) {
-            return res.status(404).send();
-          }
-          res.send(task);
-    }catch(e){
-        res.status(500).send();
+  const _id = req.params.id;
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send();
     }
-  
+    res.send(task);
+  } catch (e) {
+    console.log("here");
+    res.status(500).send();
+  }
+});
+
+// Setup updating endpoint - update task by id
+app.patch("/tasks/:id", async (req, res) => {
+  // Send error if the update is unknown
+  const updates = Object.keys(req.body);
+  const allowedUpdated = ["description", "completed"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdated.includes(update)
+  );
+  if (!isValidOperation) {
+    res.status(400).send("Error - Invalid update!");
+  }
+  const _id = req.params.id;
+  try {
+    const task = await Task.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
 app.listen(port, () => {
